ebc7fd2c38a0b81a6ed9859763ae2a3104dc3765
Esraa Mohamed <esraamohamed@Esraa-Realme5Pro.local>
2024-01-10 15:12:39 +0200

    adding JoinSessionAPI

--- src/test/java/studentClasses/TestBase.java
+++ src/test/java/studentClasses/TestBase.java
@@ -1,5 +1,6 @@
 package studentClasses;
 
+import io.cucumber.java.en.Given;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,21 +10,29 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class TestBase {
-
 	public static String access_token;
 	public  static  String student_Id = "123456789011";
 	public  static  String class_Id = "270160879432";
+	public static  String class_id_for_join_session = "102345678999";
+	public static String fully_Paid_class = "123452349098";
+	public static String fully_Paid_class_Session = "123451234562";
+	public static String session_id = "123451234566";
+	public static String expensive_session_id ="556787678976";
+	public static String ended_Session = "978609054123";
+	public static String Not_Started_Session = "786905435678";
 	public  static  String class_Title = "Mathematics";
+	public static String NotActive_student_Id = "430192963192";
 	public  static  String refresh_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcl9pZCI6IjEyMzQ1Njc4OTAxMSIsInJvbGUiOiJzdHVkZW50In0sImV4cCI6MTcxNDU1NDA5Ni41MzM2NjQsInR5cGUiOiJyZWZyZXNoIiwianRpIjoiZDNjYjdmMzQ5OTZiNDljN2FmYzEzYmU2ZTI4ZTJiOTgifQ.ACMZH37FekpeR89hkZrVLfq1abvkpm12I01YJdZL1Ek";
+	public static String Not_Activate_Student_Refresh_Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcl9pZCI6NDMwMTkyOTYzMTkyLCJyb2xlIjoic3R1ZGVudCJ9LCJleHAiOjE3MTUxNjA2NjkuODU3NzIsInR5cGUiOiJyZWZyZXNoIiwianRpIjoiNGUwMjNhODFjYzRhNGIxNGIyNWQ2MDFhZTQzZjFlNWYifQ.srh63EoINMYdaFzVVzRxG0qATPD7sW30OvCTgo_0E8U";
 
-	//DataBase connection 
+	//DataBase connection
 	public final String host = "jdbc:postgresql://nagwa-classes-beta.cluster-c4iigfolsbo7.us-east-1.rds.amazonaws.com:5432/nagwa_classes";
     public final String user = "testing_readwrite";
     public final String password = "8yZ%`6!e?~0q6<MM?hHO";
-	
-	@DisplayName("Generate Token")
-	@BeforeAll
-	public static void generate_access_token() {
+
+
+	@Given("Send {string} To Generate Token For The User And Set BaseURL")
+	public static String  generate_access_token( String refresh_token ) {
 		RestAssured.baseURI ="https://aevkujc65i.execute-api.us-east-1.amazonaws.com";
 		RestAssured.basePath ="/beta/v1";
 		RequestSpecification request = 
@@ -33,9 +42,10 @@ public class TestBase {
 
 			Response response = request.when()
 			.post("/token/refresh");
-						
-		    access_token = response.then().extract().path("access_token");
 
-  
+		return access_token = response.then().extract().path("access_token");
 	}
+
+
+
 }
