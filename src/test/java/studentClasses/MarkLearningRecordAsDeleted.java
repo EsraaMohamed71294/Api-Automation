d72cae3304cab0e7c10526009c0f81ccc624cb72
User <user@Osamas-iMac.local>
2024-01-18 14:24:39 +0200

    adding two apis

---
+++ src/test/java/studentClasses/MarkLearningRecordAsDeleted.java
@@ -0,0 +1,102 @@
+package studentClasses;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+
+public class MarkLearningRecordAsDeleted {
+    TestBase test = new TestBase();
+    TestData data = new TestData();
+    String student_id = data.student_Id;
+    String class_id = data.class_id_for_join_session;
+    String session_id = data.session_id;
+    Database_Connection Connect = new Database_Connection();
+    String archived_class = data.Archived_Class;
+    Integer newStudentLearningRecordId;
+    Integer  studentLearningRecordId;
+    Map<String, Object> PathParams = test.pathParams;
+    Response Mark_Student_Learning_Record_As_Deleted;
+    public Integer get_data() {
+        ResultSet resultSet = Connect.connect_to_database("select * from public.student_learning_records slr " +
+                "join sessions_educational_resources ser " +
+                "on slr.session_educational_resource_id = slr.session_educational_resource_id \n" +
+                "where slr.student_id = 123456789011 and educational_resource_id = 123456543221 and slr.student_learning_record_is_deleted = false and ser.session_id = 123451234566\n");
+        try {
+            while (resultSet.next()) {
+                newStudentLearningRecordId = resultSet.getInt("student_learning_record_id");
+                if (newStudentLearningRecordId != null) {
+                    studentLearningRecordId = newStudentLearningRecordId;
+                    return studentLearningRecordId;
+                }else{
+                    return  newStudentLearningRecordId;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    @When("Performing The API Of Mark Learning Record As Deleted API")
+    public void perform_mark_learning_record_as_deleted() {
+        Mark_Student_Learning_Record_As_Deleted = test.sendRequest("DELETE", "/students/{student_id}/classes/{class_id}/sessions/{session_id}/records/{record_id}", null);
+    }
+    @Given("User Send Valid Data To MarkLearningRecordAsDeleted API")
+    public void delete_student_learning_record() {
+        PathParams.put("student_id", student_id);
+        PathParams.put("class_id", class_id);
+        PathParams.put("session_id", session_id);
+        PathParams.put("record_id", get_data().toString());
+    }
+    @Then("Response Status Code Is 200 And Body Contains Learning Record Is Deleted Successfully")
+    public void validate_mark_learning_Record_response() {
+        Mark_Student_Learning_Record_As_Deleted.prettyPrint();
+        Mark_Student_Learning_Record_As_Deleted.then()
+                .statusCode(HttpStatus.SC_OK)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("/Users/user/Api_Automation/src/test/resources/Schemas/MarkLearningRecordAsDeleted.json")))
+                .body("message", containsString("Learning record successfully marked as deleted"))
+                .body("record_id", equalTo((studentLearningRecordId)));
+    }
+    @Given("User Send Invalid Student Id To Mark Learning Record Request")
+    public void mark_learning_record_unAuthorizedStudent() {
+        PathParams.put("student_id", "134565433123");
+        PathParams.put("class_id", class_id);
+        PathParams.put("session_id", session_id);
+        PathParams.put("record_id", get_data().toString());
+    }
+    @Then("Status Code Of Mark Request Is 403 And Body Have Error Message")
+    public void validate_mark_learning_Record_unauthorized() {
+        test.Validate_Error_Messages(Mark_Student_Learning_Record_As_Deleted, HttpStatus.SC_FORBIDDEN, "Unauthorized", 4031);
+    }
+    @Given("User Send Archived ClassId To Mark Learning Record As Deleted API")
+    public void mark_learning_Record_invalid_class() {
+        PathParams.put("student_id", student_id);
+        PathParams.put("class_id", archived_class);
+        PathParams.put("session_id", session_id);
+        PathParams.put("record_id", get_data().toString());
+    }
+    @Then("Response Code is 404 And Body Have Class Not Found Message")
+    public void validate_mark_learning_record_invalid_classId() {
+        test.Validate_Error_Messages(Mark_Student_Learning_Record_As_Deleted, HttpStatus.SC_NOT_FOUND, "Class not found or not eligible for display", 4046);
+    }
+    @Given("Send Deleted Learning Record")
+    public void mark_learning_Record_marked_as_deleted() {
+        PathParams.put("student_id", student_id);
+        PathParams.put("class_id", class_id);
+        PathParams.put("session_id", session_id);
+        PathParams.put("record_id", get_data().toString());
+    }
+    @Then("Response Code Is 404 And Body Have Learning Record Is Deleted Message")
+    public void validate_deleted_learning_record() {
+        test.Validate_Error_Messages(Mark_Student_Learning_Record_As_Deleted, HttpStatus.SC_NOT_FOUND, "Learning record not found or has been deleted.", 40411);
+    }
+}
--- src/test/java/studentClasses/SubmitStudentLearningRecord.java
+++ src/test/java/studentClasses/SubmitStudentLearningRecord.java
@@ -1,4 +1,89 @@
 package studentClasses;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+
 public class SubmitStudentLearningRecord {
+    TestBase test = new TestBase();
+    TestData data = new TestData();
+    String student_id = data.student_Id;
+    String class_id =   data.class_id_for_join_session;
+    String session_id = data.session_id;
+    String invalid_request_body = null;
+    Long resource_id = Long.valueOf(data.resource_id);
+    Integer Student_learning_Record_id = data.Student_Learning_Record_Id;
+    Map<String,Object> PathParams = test.pathParams;
+    Response Submit_Student_Learning_Record;
+    String request_body = "{\"student_learning_record\":\"{\\\"hello\\\":\\\"world\\\"}\",\"student_learning_record_metadata\":\"{\\\"helloagain\\\":\\\"world\\\"}\"}";
+    @When("Performing The API Of SubmitStudentLearningRecord")
+    public void submit_student_learning_Record_request(){
+        Submit_Student_Learning_Record = test.sendRequest("POST", "/students/{student_id}/classes/{class_id}/sessions/{session_id}/resources/{resource_id}/record\n" +
+                "\n" , request_body);
+    }
+    @Given("User Send Valid Data To SubmitStudentLearningRecord Request")
+    public void submit_student_learning_record(){
+        PathParams.put("student_id", student_id);
+        PathParams.put("class_id",class_id);
+        PathParams.put("session_id",session_id);
+        PathParams.put("resource_id",resource_id);
+    }
+    @Then("Response Status Code Is 200 And Body Have StudentLearning RecordId")
+    public void validate_student_learning_Record_response(){
+        Submit_Student_Learning_Record.prettyPrint();
+        Submit_Student_Learning_Record.then().assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("/Users/user/Api_Automation/src/test/resources/Schemas/SubmitStudentLearningRecord.json")))
+                .body("educational_resource_id",equalTo(Student_learning_Record_id));
+    }
+   @Given("User Send Invalid UserId To SubmitStudentLearningRecord Request")
+    public void invalid_user_submit_student_learning_record(){
+        PathParams.put("student_id", "109876547890");
+        PathParams.put("class_id",class_id);
+        PathParams.put("session_id",session_id);
+        PathParams.put("resource_id",resource_id);
+    }
+    @Then("Response Status Code Of SubmitStudentLearningRecord Is 403 and Body Have Error Message")
+    public void unauthorized_student_in_submit_learning_record(){
+        test.Validate_Error_Messages(Submit_Student_Learning_Record, HttpStatus.SC_FORBIDDEN , "Unauthorized",4031);
+    }
+  @Given("User Send Invalid SessionId To SubmitStudentLearningRecord Request")
+    public void invalid_session_id(){
+        PathParams.put("student_id", student_id);
+        PathParams.put("class_id",class_id);
+        PathParams.put("session_id","123456765432");
+        PathParams.put("resource_id",resource_id);
+    }
+    @Then("Response Status Code Is 403 And Error Message Contains No Access To Session")
+    public void validate_invalid_session_id(){
+        test.Validate_Error_Messages(Submit_Student_Learning_Record , HttpStatus.SC_FORBIDDEN ,"Unauthorized access. Student does not have access to the resources of the requested session or class", 4036 );
+    }
+    @Given("User Send Valid Parameters And Empty Body")
+    public void send_invalid_body(){
+        PathParams.put("student_id", student_id);
+        PathParams.put("class_id",class_id);
+        PathParams.put("session_id",session_id);
+        PathParams.put("resource_id",resource_id);
+    }
+    @When("Performing The API With Empty Body")
+    public void Execute_Request_With_Empty_Body(){
+        Submit_Student_Learning_Record = test.sendRequest("POST", "/students/{student_id}/classes/{class_id}/sessions/{session_id}/resources/{resource_id}/record\n" +
+                "\n" ,invalid_request_body );
+    }
+    @Then("Response Code Is 400 And Body Have Clear Error Message")
+    public void Validate_invalid_body_Request(){
+        test.Validate_Error_Messages(Submit_Student_Learning_Record, HttpStatus.SC_BAD_REQUEST , "Invalid request. Please check the path parameters and request context for accuracy", 4002);
+    }
 }
