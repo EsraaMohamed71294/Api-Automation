cae73d6f0a6956f6d9a7a64af4080f639b8d6c54
Esraa Mohamed <esraamohamed@Esraa-Realme5Pro.local>
2024-01-24 14:38:40 +0200

    uploading create educator

--- src/test/java/TestReport/TestReport_StudentClasses.java
+++ src/test/java/TestReport/TestReport_StudentClasses.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Features/StudentClassesFeatures"
                 ,glue = "studentClasses"
-                , plugin = {"pretty", "html:target/TestReport_StudentClasses.html"})
+                , plugin = {"pretty", "html:target/Reports/TestReport_StudentClasses.html"})
 
 public class TestReport_StudentClasses extends AbstractTestNGCucumberTests {
 
