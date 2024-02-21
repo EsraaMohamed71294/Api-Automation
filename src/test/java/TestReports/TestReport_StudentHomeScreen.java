d4acd6e63af6d27ad78b26228063271eef65010e
Esraa Mohamed <esraamohamed@Esraa-Realme5Pro.local>
2024-02-21 10:28:21 +0200

    reports

--- src/test/java/TestReports/TestReport_StudentHomeScreen.java
+++ src/test/java/TestReports/TestReport_StudentHomeScreen.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Features/StudentHomeScreen"
-        ,glue = "StudentHomeScreen"
-        , plugin = {"pretty", "html:target/Reports/TestReport_StudentHomeScreen.html"})
+                ,glue = "StudentHomeScreen"
+                , plugin = {"pretty", "html:target/Reports/TestReport_StudentHomeScreen.html"})
 
 public class TestReport_StudentHomeScreen extends AbstractTestNGCucumberTests {
 }
--- src/test/java/TestReports/TestReport_StudentParentAuth.java
+++ src/test/java/TestReports/TestReport_StudentParentAuth.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Features/StudentParentAuth"
-        ,glue = "StudentParentAuth"
-        , plugin = {"pretty", "html:target/Reports/TestReport_StudentParentAuth.html"})
+                ,glue = "StudentParentAuth"
+                , plugin = {"pretty", "html:target/Reports/TestReport_StudentParentAuth.html"})
 
 
 public class TestReport_StudentParentAuth extends AbstractTestNGCucumberTests {
