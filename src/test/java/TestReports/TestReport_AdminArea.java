5842a9a15c376f140700079d958df619589f5a79
Esraa Mohamed <esraamohamed@Esraa-Realme5Pro.local>
2024-02-29 10:59:47 +0200

    reports

--- src/test/java/TestReports/TestReport_AdminArea.java
+++ src/test/java/TestReports/TestReport_AdminArea.java
@@ -4,9 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/resources/Features/AdminArea"}
-                ,glue = "AdminArea"
-                ,plugin = {"pretty", "html:target/Reports/TestRunner.html"})
+        ,glue = "AdminArea"
+        ,plugin = {"pretty", "html:target/Reports/TestReport_AdminArea.html"})
 
-public class TestRunner extends AbstractTestNGCucumberTests {
-
+public class TestReport_AdminArea extends AbstractTestNGCucumberTests {
 }
