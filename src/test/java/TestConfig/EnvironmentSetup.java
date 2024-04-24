2b820d8536b2d8fe85eca1d4d0c457c696787922
EsraaMohamed71294 <162455924+EsraaMohamed71294@users.noreply.github.com>
2024-04-24 13:37:21 +0200

    Update EnvironmentSetup.java

--- src/test/java/TestConfig/EnvironmentSetup.java
+++ src/test/java/TestConfig/EnvironmentSetup.java
@@ -5,26 +5,6 @@ import io.restassured.RestAssured;
 public class EnvironmentSetup {
 
     public static String env = "DEMO";
-    // env :{ "DEMO", "BETA","LIVE"}
-
-    public static void setEnvironment(String environment) {
-        switch (environment) {
-            case "DEMO":
-                RestAssured.baseURI = "https://demo-api.nagwa.io";
-                RestAssured.basePath = "/v1";
-                break;
-            case "BETA":
-                RestAssured.baseURI = "https://beta-api.nagwa.io";
-                RestAssured.basePath = "/v1";
-                break;
-            case "LIVE":
-                RestAssured.baseURI = "https://api.nagwa.io";
-                RestAssured.basePath = "/v1";
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid environment: " + environment);
-        }
-    }
-
+    //set env :{ "DEMO", "BETA","LIVE"}
 
 }
